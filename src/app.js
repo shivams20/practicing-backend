8594e493d7d1e2328df2f3db399f6c62e1028117
shivams20 <109885021+shivams20@users.noreply.github.com>
2024-06-11 11:00:16 +0530

    add user and video model

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+// Data can come in any form like json, url, form etc...
+// To convert data into json (the data which is coming)
+app.use(express.json({limit: "16kb"}))
+// When daat comes into form of URL. extended means you can give objects inside objects(nested objects)
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+//When we want to store some files, folders, pdf, images, so we make public folder that public assets are there so anybody can access it...
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-
+import app from "./app.js";
 dotenv.config({
     path: './env'
 })
@@ -8,7 +8,15 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+})
 
 // (async () => {
 //     try{
